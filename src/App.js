82f9f230785aed7e4f994ed1f6396fa7bb6341a2
CosiBerger <Cosima.Berger@web.de>
2020-01-24 16:15:23 +0100

    Überflüssige Importe aus der App.js entfernt

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import LotteryTicket from './LotteryTicket';
+import React from "react";
+import "./App.css";
+import LotteryTicket from "./LotteryTicket";
 
 class App extends React.Component {
-  
-  render (){
-    return(
-      <LotteryTicket />
-    );
+  render() {
+    return <LotteryTicket />;
   }
-    
 }
 
 export default App;
